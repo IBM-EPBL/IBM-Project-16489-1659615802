e41f3d85f1137ea3acc6df0c31312c020a1db2a5
BaumaRanjith <104650653+BaumaRanjith@users.noreply.github.com>
2022-11-20 10:39:29 +0530

    Add files via upload

---
+++ ASSIGNMENT/BAUMA RANJITH/ASSIGNMENT 2/assignment2/app.py
@@ -0,0 +1,27 @@
+from flask import Flask,render_template,request
+app = Flask(__name__)
+@app.route('/')
+@app.route('/register',methods=['GET','POST'])
+def register():
+      if request.method == "POST":
+          username = request.form.get('username')
+          email = request.form.get('email')
+          phone = request.form.get('phone')
+
+          return display_info(username,email,phone)
+      return render_template('registerassign.html')
+      
+def display_info(name,mail,phone):
+      return f'''
+          <center>
+           <h2> Details: </h2>
+           <p> username : {name} </p>
+           <p> email : {mail} </p>
+           <p> phone : {phone} </p>
+          </center>
+      
+      '''  
+if __name__==('__main__'):
+    app.run(debug=True)   
+    
+    
